2c64e980b8bb65542938aaceb9cfd06b90e27a98
Trevor Saunders <tbsaunde@tbsaunde.org>
2021-06-11 23:49:22 -0400

    use range based for loops to iterate over vec<>
    This changes users of FOR_EACH_VEC_ELT to use range based for loops,
    where the index variables are otherwise unused.  As such the index
    variables are all deleted, producing shorter and simpler code.
    
    Signed-off-by: Trevor Saunders <tbsaunde@tbsaunde.org>
    
    gcc/analyzer/ChangeLog:
    
    	* call-string.cc (call_string::call_string): Use range based for
    	to iterate over vec<>.
    	(call_string::to_json): Likewise.
    	(call_string::hash): Likewise.
    	(call_string::calc_recursion_depth): Likewise.
    	* checker-path.cc (checker_path::fixup_locations): Likewise.
    	* constraint-manager.cc (equiv_class::equiv_class): Likewise.
    	(equiv_class::to_json): Likewise.
    	(equiv_class::hash): Likewise.
    	(constraint_manager::to_json): Likewise.
    	* engine.cc (impl_region_model_context::on_svalue_leak):
    	Likewise.
    	(on_liveness_change): Likewise.
    	(impl_region_model_context::on_unknown_change): Likewise.
    	* program-state.cc (sm_state_map::set_state): Likewise.
    	* region-model.cc (test_canonicalization_4): Likewise.
    
    gcc/ChangeLog:
    
    	* attribs.c (find_attribute_namespace): Iterate over vec<> with
    	range based for.
    	* auto-profile.c (afdo_find_equiv_class): Likewise.
    	* gcc.c (do_specs_vec): Likewise.
    	(do_spec_1): Likewise.
    	(driver::set_up_specs): Likewise.
    	* gimple-loop-jam.c (any_access_function_variant_p): Likewise.
    	* gimple-ssa-store-merging.c (compatible_load_p): Likewise.
    	(imm_store_chain_info::try_coalesce_bswap): Likewise.
    	(imm_store_chain_info::coalesce_immediate_stores): Likewise.
    	(get_location_for_stmts): Likewise.
    	* graphite-poly.c (print_iteration_domains): Likewise.
    	(free_poly_bb): Likewise.
    	(remove_gbbs_in_scop): Likewise.
    	(free_scop): Likewise.
    	(dump_gbb_cases): Likewise.
    	(dump_gbb_conditions): Likewise.
    	(print_pdrs): Likewise.
    	(print_scop): Likewise.
    	* ifcvt.c (cond_move_process_if_block): Likewise.
    	* lower-subreg.c (decompose_multiword_subregs): Likewise.
    	* regcprop.c (pass_cprop_hardreg::execute): Likewise.
    	* sanopt.c (sanitize_rewrite_addressable_params): Likewise.
    	* sel-sched-dump.c (dump_insn_vector): Likewise.
    	* store-motion.c (store_ops_ok): Likewise.
    	(store_killed_in_insn): Likewise.
    	* timevar.c (timer::named_items::print): Likewise.
    	* tree-cfgcleanup.c (cleanup_control_flow_pre): Likewise.
    	(cleanup_tree_cfg_noloop): Likewise.
    	* tree-data-ref.c (dump_data_references): Likewise.
    	(print_dir_vectors): Likewise.
    	(print_dist_vectors): Likewise.
    	(dump_data_dependence_relations): Likewise.
    	(dump_dist_dir_vectors): Likewise.
    	(dump_ddrs): Likewise.
    	(create_runtime_alias_checks): Likewise.
    	(free_subscripts): Likewise.
    	(save_dist_v): Likewise.
    	(save_dir_v): Likewise.
    	(invariant_access_functions): Likewise.
    	(same_access_functions): Likewise.
    	(access_functions_are_affine_or_constant_p): Likewise.
    	(find_data_references_in_stmt): Likewise.
    	(graphite_find_data_references_in_stmt): Likewise.
    	(free_dependence_relations): Likewise.
    	(free_data_refs): Likewise.
    	* tree-inline.c (copy_debug_stmts): Likewise.
    	* tree-into-ssa.c (dump_currdefs): Likewise.
    	(rewrite_update_phi_arguments): Likewise.
    	* tree-ssa-propagate.c (clean_up_loop_closed_phi): Likewise.
    	* tree-vect-data-refs.c (vect_analyze_possibly_independent_ddr):
    	Likewise.
    	(vect_slp_analyze_node_dependences): Likewise.
    	(vect_slp_analyze_instance_dependence): Likewise.
    	(vect_record_base_alignments): Likewise.
    	(vect_get_peeling_costs_all_drs): Likewise.
    	(vect_peeling_supportable): Likewise.
    	* tree-vectorizer.c (vec_info::~vec_info): Likewise.
    	(vec_info::free_stmt_vec_infos): Likewise.
    
    gcc/cp/ChangeLog:
    
    	* constexpr.c (cxx_eval_call_expression): Iterate over vec<>
    	with range based for.
    	(cxx_eval_store_expression): Likewise.
    	(cxx_eval_loop_expr): Likewise.
    	* decl.c (wrapup_namespace_globals): Likewise.
    	(cp_finish_decl): Likewise.
    	(cxx_simulate_enum_decl): Likewise.
    	* parser.c (cp_parser_postfix_expression): Likewise.

--- gcc/gimple-ssa-store-merging.cc
+++ gcc/gimple-ssa-store-merging.cc
@@ -2559,8 +2559,6 @@ compatible_load_p (merged_store_group *merged_store,
      clobbers those loads.  */
   gimple *first = merged_store->first_stmt;
   gimple *last = merged_store->last_stmt;
-  unsigned int i;
-  store_immediate_info *infoc;
   /* The stores are sorted by increasing store bitpos, so if info->stmt store
      comes before the so far first load, we'll be changing
      merged_store->first_stmt.  In that case we need to give up if
@@ -2568,7 +2566,7 @@ compatible_load_p (merged_store_group *merged_store,
      range.  */
   if (info->order < merged_store->first_order)
     {
-      FOR_EACH_VEC_ELT (merged_store->stores, i, infoc)
+      for (store_immediate_info *infoc : merged_store->stores)
 	if (stmts_may_clobber_ref_p (info->stmt, first, infoc->ops[idx].val))
 	  return false;
       first = info->stmt;
@@ -2578,7 +2576,7 @@ compatible_load_p (merged_store_group *merged_store,
      processed loads.  */
   else if (info->order > merged_store->last_order)
     {
-      FOR_EACH_VEC_ELT (merged_store->stores, i, infoc)
+      for (store_immediate_info *infoc : merged_store->stores)
 	if (stmts_may_clobber_ref_p (last, info->stmt, infoc->ops[idx].val))
 	  return false;
       last = info->stmt;
@@ -2902,9 +2900,7 @@ imm_store_chain_info::try_coalesce_bswap (merged_store_group *merged_store,
 	gather_bswap_load_refs (&refs,
 				gimple_assign_rhs1 (m_store_info[i]->stmt));
 
-      unsigned int i;
-      tree ref;
-      FOR_EACH_VEC_ELT (refs, i, ref)
+      for (tree ref : refs)
 	if (stmts_may_clobber_ref_p (first_stmt, last_stmt, ref))
 	  return false;
       n.vuse = NULL_TREE;
@@ -3217,9 +3213,7 @@ imm_store_chain_info::coalesce_immediate_stores ()
 		}
 	      else if (infof->rhs_code == MEM_REF && info->rhs_code != MEM_REF)
 		{
-		  store_immediate_info *infoj;
-		  unsigned int j;
-		  FOR_EACH_VEC_ELT (merged_store->stores, j, infoj)
+		  for (store_immediate_info *infoj : merged_store->stores)
 		    {
 		      infoj->rhs_code = BIT_INSERT_EXPR;
 		      infoj->ops[0].val = gimple_assign_rhs1 (infoj->stmt);
@@ -3341,10 +3335,7 @@ get_alias_type_for_stmts (vec<gimple *> &stmts, bool is_load,
 static location_t
 get_location_for_stmts (vec<gimple *> &stmts)
 {
-  gimple *stmt;
-  unsigned int i;
-
-  FOR_EACH_VEC_ELT (stmts, i, stmt)
+  for (gimple *stmt : stmts)
     if (gimple_has_location (stmt))
       return gimple_location (stmt);
 
