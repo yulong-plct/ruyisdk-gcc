d67b1566f24da99371f235d54411b5de0994e31f
Tamar Christina <tamar.christina@arm.com>
2021-08-06 12:21:05 +0100

    middle-end/AArch64: Fix bootstrap after vec changes
    The build is broken since a3d3e8c362c2 since it's deleted the ability to pass
    vec<> by value and now must be past by reference.
    
    However some language hooks used by AArch64 were not updated and breaks the
    build on AArch64.  This patch updates these hooks.
    
    gcc/c/ChangeLog:
    
    	* c-decl.c (c_simulate_enum_decl): Pass vec<> by pointer.
    	* c-tree.h (c_simulate_enum_decl): Likewise.
    
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64-sve-builtins.cc (register_svpattern,
    	register_svprfop): Pass vec<> by pointer.
    	* langhooks-def.h (lhd_simulate_enum_decl): Likewise.
    	* langhooks.c (lhd_simulate_enum_decl): Likewise.
    	* langhooks.h (struct lang_hooks_for_types): Likewise.
    
    gcc/cp/ChangeLog:
    
    	* cp-objcp-common.h (cxx_simulate_enum_decl): Pass vec<> by pointer.
    	* decl.c (cxx_simulate_enum_decl): Likewise.

--- gcc/c/c-decl.cc
+++ gcc/c/c-decl.cc
@@ -9367,7 +9367,7 @@ build_enumerator (location_t decl_loc, location_t loc,
 
 tree
 c_simulate_enum_decl (location_t loc, const char *name,
-		      vec<string_int_pair> values)
+		      vec<string_int_pair> *values_ptr)
 {
   location_t saved_loc = input_location;
   input_location = loc;
@@ -9377,6 +9377,7 @@ c_simulate_enum_decl (location_t loc, const char *name,
 
   tree value_chain = NULL_TREE;
   string_int_pair *value;
+  vec<string_int_pair> values = *values_ptr;
   unsigned int i;
   FOR_EACH_VEC_ELT (values, i, value)
     {
