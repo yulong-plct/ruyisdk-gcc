90a2203600b762bf6546435643269ff776555039
Andrew MacLeod <amacleod@redhat.com>
2023-04-13 14:47:47 -0400

    Add sbr_lazy_vector and adjust (e)vrp sparse cache
    Add a sparse vector class for cache and use if by default.
    Rename the evrp_* params to vrp_*, and add a param for small CFGS which use
    just the original basic vector.
    
    	* gimple-range-cache.cc (sbr_vector::sbr_vector): Add parameter
    	and local to optionally zero memory.
    	(br_vector::grow): Only zero memory if flag is set.
    	(class sbr_lazy_vector): New.
    	(sbr_lazy_vector::sbr_lazy_vector): New.
    	(sbr_lazy_vector::set_bb_range): New.
    	(sbr_lazy_vector::get_bb_range): New.
    	(sbr_lazy_vector::bb_range_p): New.
    	(block_range_cache::set_bb_range): Check flags and Use sbr_lazy_vector.
    	* gimple-range-gori.cc (gori_map::calculate_gori): Use
    	param_vrp_switch_limit.
    	(gori_compute::gori_compute): Use param_vrp_switch_limit.
    	* params.opt (vrp_sparse_threshold): Rename from evrp_sparse_threshold.
    	(vrp_switch_limit): Rename from evrp_switch_limit.
    	(vrp_vector_threshold): New.

--- gcc/gimple-range-gori.cc
+++ gcc/gimple-range-gori.cc
@@ -576,6 +576,9 @@ gori_map::calculate_gori (basic_block bb)
     }
   else
     {
+      // Do not process switches if they are too large.
+      if (EDGE_COUNT (bb->succs) > (unsigned)param_vrp_switch_limit)
+	return;
       gswitch *gs = as_a<gswitch *>(stmt);
       name = gimple_range_ssa_p (gimple_switch_index (gs));
       maybe_add_gori (name, gimple_bb (stmt));
@@ -645,7 +648,8 @@ debug (gori_map &g)
 
 // Construct a gori_compute object.
 
-gori_compute::gori_compute ()
+gori_compute::gori_compute (int not_executable_flag)
+		      : outgoing (param_vrp_switch_limit), tracer ("GORI ")
 {
   // Create a boolean_type true and false range.
   m_bool_zero = int_range<2> (boolean_false_node, boolean_false_node);
