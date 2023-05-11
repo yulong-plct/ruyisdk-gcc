cb20509cd1b7c84277e8992fabe331e113399d52
Pan Li <pan2.li@intel.com>
2023-05-11 18:50:56 +0800

    VECT: Add tree_code into "creat_iv" and allow it can handle MINUS_EXPR IV
    This patch is going to be commited after bootstrap && regression on X86
    PASSED.
    
    Thanks Richards.
    
    gcc/ChangeLog:
    
    	* cfgloopmanip.cc (create_empty_loop_on_edge): Add PLUS_EXPR.
    	* gimple-loop-interchange.cc
    	(tree_loop_interchange::map_inductions_to_loop): Ditto.
    	* tree-ssa-loop-ivcanon.cc (create_canonical_iv): Ditto.
    	* tree-ssa-loop-ivopts.cc (create_new_iv): Ditto.
    	* tree-ssa-loop-manip.cc (create_iv): Ditto.
    	(tree_transform_and_unroll_loop): Ditto.
    	(canonicalize_loop_ivs): Ditto.
    	* tree-ssa-loop-manip.h (create_iv): Ditto.
    	* tree-vect-data-refs.cc (vect_create_data_ref_ptr): Ditto.
    	* tree-vect-loop-manip.cc (vect_set_loop_controls_directly):
    	Ditto.
    	(vect_set_loop_condition_normal): Ditto.
    	* tree-vect-loop.cc (vect_create_epilog_for_reduction): Ditto.
    	* tree-vect-stmts.cc (vectorizable_store): Ditto.
    	(vectorizable_load): Ditto.
    
    Signed-off-by: Juzhe Zhong <juzhe.zhong@rivai.ai>

--- gcc/tree-ssa-loop-ivopts.cc
+++ gcc/tree-ssa-loop-ivopts.cc
@@ -7141,7 +7141,7 @@ create_new_iv (struct ivopts_data *data, struct iv_cand *cand)
 
   base = unshare_expr (cand->iv->base);
 
-  create_iv (base, unshare_expr (cand->iv->step),
+  create_iv (base, PLUS_EXPR, unshare_expr (cand->iv->step),
 	     cand->var_before, data->current_loop,
 	     &incr_pos, after, &cand->var_before, &cand->var_after);
 }
