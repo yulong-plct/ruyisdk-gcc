3ebb4fe67d49177e87f4c9b8e7d94781c9c35c80
Pan Li <pan2.li@intel.com>
2023-06-21 15:58:24 +0800

    Streamer: Fix out of range memory access of machine mode
    We extend the machine mode from 8 to 16 bits already. But there still
    one placing missing from the streamer. It has one hard coded array
    for the machine code like size 256.
    
    In the lto pass, we memset the array by MAX_MACHINE_MODE count but the
    value of the MAX_MACHINE_MODE will grow as more and more modes are
    added. While the machine mode array in tree-streamer still leave 256 as is.
    
    Then, when the MAX_MACHINE_MODE is greater than 256, the memset of
    lto_output_init_mode_table will touch the memory out of range unexpected.
    
    This patch would like to take the MAX_MACHINE_MODE as the size of the
    array in streamer, to make sure there is no potential unexpected
    memory access in future. Meanwhile, this patch also adjust some place
    which has MAX_MACHINE_MODE <= 256 assumption.
    
    Care is taken that for offload compilation, we interpret the stream-in
    data in terms of the host 'MAX_MACHINE_MODE' ('file_data->mode_bits'),
    which very likely is different from the offload device
    'MAX_MACHINE_MODE'.
    
    	gcc/
    	* lto-streamer-in.cc (lto_input_mode_table): Stream in the mode
    	bits for machine mode table.
    	* lto-streamer-out.cc (lto_write_mode_table): Stream out the
    	HOST machine mode bits.
    	* lto-streamer.h (struct lto_file_decl_data): New fields mode_bits.
    	* tree-streamer.cc (streamer_mode_table): Take MAX_MACHINE_MODE
    	as the table size.
    	* tree-streamer.h (streamer_mode_table): Ditto.
    	(bp_pack_machine_mode): Take 1 << ceil_log2 (MAX_MACHINE_MODE)
    	as the packing limit.
    	(bp_unpack_machine_mode): Ditto with 'file_data->mode_bits'.
    	gcc/lto/
    	* lto-common.cc (lto_file_finalize) [!ACCEL_COMPILER]: Initialize
    	'file_data->mode_bits'.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>
    Co-authored-by: Thomas Schwinge <thomas@codesourcery.com>

--- gcc/tree-streamer.h
+++ gcc/tree-streamer.h
@@ -75,7 +75,7 @@ void streamer_write_tree_body (struct output_block *, tree);
 void streamer_write_integer_cst (struct output_block *, tree);
 
 /* In tree-streamer.cc.  */
-extern unsigned char streamer_mode_table[1 << 8];
+extern unsigned char streamer_mode_table[MAX_MACHINE_MODE];
 void streamer_check_handled_ts_structures (void);
 bool streamer_tree_cache_insert (struct streamer_tree_cache_d *, tree,
 				 hashval_t, unsigned *);
@@ -108,7 +108,8 @@ static inline void
 bp_pack_machine_mode (struct bitpack_d *bp, machine_mode mode)
 {
   streamer_mode_table[mode] = 1;
-  bp_pack_enum (bp, machine_mode, 1 << 8, mode);
+  int last = 1 << ceil_log2 (MAX_MACHINE_MODE);
+  bp_pack_enum (bp, machine_mode, last, mode);
 }
 
 static inline machine_mode
