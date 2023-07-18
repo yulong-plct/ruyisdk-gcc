c0b32e5b34f07604a7d9d3916945c1301bdd81b7
Richard Biener <rguenther@suse.de>
2023-07-18 13:19:11 +0200

    middle-end/61747 - conditional move expansion and constants
    When expanding a COND_EXPR or a VEC_COND_EXPR the x86 backend for
    example tries to match FP min/max instructions.  But this only
    works when it can see the equality of the comparison and selected
    operands.  This breaks in both prepare_cmp_insn and vector_compare_rtx
    where the former forces expensive constants to a register and the
    latter performs legitimization.  The patch below fixes this in
    the caller preserving former equalities.
    
    	PR middle-end/61747
    	* internal-fn.cc (expand_vec_cond_optab_fn): When the
    	value operands are equal to the original comparison operands
    	preserve that equality by re-using the comparison expansion.
    	* optabs.cc (emit_conditional_move): When the value operands
    	are equal to the comparison operands and would be forced to
    	a register by prepare_cmp_insn do so earlier, preserving the
    	equality.
    
    	* g++.target/i386/pr61747.C: New testcase.

--- gcc/optabs.cc
+++ gcc/optabs.cc
@@ -4891,24 +4891,44 @@ emit_conditional_move (rtx target, enum rtx_code code, rtx op0, rtx op1,
 	  save_pending_stack_adjust (&save);
 	  last = get_last_insn ();
 	  do_pending_stack_adjust ();
-	  machine_mode cmpmode = cmode;
+	  machine_mode cmpmode = comp.mode;
+	  rtx orig_op0 = XEXP (comparison, 0);
+	  rtx orig_op1 = XEXP (comparison, 1);
+	  rtx op2p = op2;
+	  rtx op3p = op3;
+	  /* If we are optimizing, force expensive constants into a register
+	     but preserve an eventual equality with op2/op3.  */
+	  if (CONSTANT_P (orig_op0) && optimize
+	      && (rtx_cost (orig_op0, mode, COMPARE, 0,
+			    optimize_insn_for_speed_p ())
+		  > COSTS_N_INSNS (1))
+	      && can_create_pseudo_p ())
+	    {
+	      if (rtx_equal_p (orig_op0, op2))
+		op2p = XEXP (comparison, 0) = force_reg (cmpmode, orig_op0);
+	      else if (rtx_equal_p (orig_op0, op3))
+		op3p = XEXP (comparison, 0) = force_reg (cmpmode, orig_op0);
+	    }
+	  if (CONSTANT_P (orig_op1) && optimize
+	      && (rtx_cost (orig_op1, mode, COMPARE, 0,
+			    optimize_insn_for_speed_p ())
+		  > COSTS_N_INSNS (1))
+	      && can_create_pseudo_p ())
+	    {
+	      if (rtx_equal_p (orig_op1, op2))
+		op2p = XEXP (comparison, 1) = force_reg (cmpmode, orig_op1);
+	      else if (rtx_equal_p (orig_op1, op3))
+		op3p = XEXP (comparison, 1) = force_reg (cmpmode, orig_op1);
+	    }
 	  prepare_cmp_insn (XEXP (comparison, 0), XEXP (comparison, 1),
 			    GET_CODE (comparison), NULL_RTX, unsignedp,
 			    OPTAB_WIDEN, &comparison, &cmpmode);
 	  if (comparison)
 	    {
-	      class expand_operand ops[4];
-
-	      create_output_operand (&ops[0], target, mode);
-	      create_fixed_operand (&ops[1], comparison);
-	      create_input_operand (&ops[2], op2, mode);
-	      create_input_operand (&ops[3], op3, mode);
-	      if (maybe_expand_insn (icode, 4, ops))
-		{
-		  if (ops[0].value != target)
-		    convert_move (target, ops[0].value, false);
-		  return target;
-		}
+	       rtx res = emit_conditional_move_1 (target, comparison,
+						  op2p, op3p, mode);
+	       if (res != NULL_RTX)
+		 return res;
 	    }
 	  delete_insns_since (last);
 	  restore_pending_stack_adjust (&save);
