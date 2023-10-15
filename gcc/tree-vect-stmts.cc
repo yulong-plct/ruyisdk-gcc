caa3807c66dd9c1b218ee10879b401d85005f632
Robin Dapp <rdapp@ventanamicro.com>
2023-10-15 22:36:59 +0200

    vect: Allow same precision for bit-precision conversions.
    In PR111794 we miss a vectorization because on riscv type precision and
    mode precision differ for mask types.  We can still vectorize when
    allowing assignments with the same precision for dest and source which
    is what this patch does.
    
    gcc/ChangeLog:
    
    	PR tree-optimization/111794
    	* tree-vect-stmts.cc (vectorizable_assignment): Add
    	same-precision exception for dest and source.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/autovec/slp-mask-1.c: New test.
    	* gcc.target/riscv/rvv/autovec/slp-mask-run-1.c: New test.

--- gcc/tree-vect-stmts.cc
+++ gcc/tree-vect-stmts.cc
@@ -5928,14 +5928,18 @@ vectorizable_assignment (vec_info *vinfo,
       && (!type_has_mode_precision_p (TREE_TYPE (scalar_dest))
 	  || !type_has_mode_precision_p (TREE_TYPE (op)))
       /* But a conversion that does not change the bit-pattern is ok.  */
-      && !((TYPE_PRECISION (TREE_TYPE (scalar_dest))
-	    > TYPE_PRECISION (TREE_TYPE (op)))
-	   && TYPE_UNSIGNED (TREE_TYPE (op))))
+      && !(INTEGRAL_TYPE_P (TREE_TYPE (scalar_dest))
+	   && INTEGRAL_TYPE_P (TREE_TYPE (op))
+	   && (((TYPE_PRECISION (TREE_TYPE (scalar_dest))
+	       > TYPE_PRECISION (TREE_TYPE (op)))
+	     && TYPE_UNSIGNED (TREE_TYPE (op)))
+	       || (TYPE_PRECISION (TREE_TYPE (scalar_dest))
+		   == TYPE_PRECISION (TREE_TYPE (op))))))
     {
       if (dump_enabled_p ())
-        dump_printf_loc (MSG_MISSED_OPTIMIZATION, vect_location,
-                         "type conversion to/from bit-precision "
-                         "unsupported.\n");
+	dump_printf_loc (MSG_MISSED_OPTIMIZATION, vect_location,
+			 "type conversion to/from bit-precision "
+			 "unsupported.\n");
       return false;
     }
 
