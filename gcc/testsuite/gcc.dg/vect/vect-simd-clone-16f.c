e9d3c4b80820afa9d7948bfef3e0672b35c20b9a
Andre Vieira <andre.simoesdiasvieira@arm.com>
2023-10-19 18:30:15 +0100

    vect: Fix vect_get_smallest_scalar_type for simd clones
    The vect_get_smallest_scalar_type helper function was using any argument to a
    simd clone call when trying to determine the smallest scalar type that would be
    vectorized.  This included the function pointer type in a MASK_CALL for
    instance, and would result in the wrong type being selected.  Instead this
    patch special cases simd_clone_call's and uses only scalar types of the
    original function that get transformed into vector types.
    
    gcc/ChangeLog:
    
    	* tree-vect-data-refs.cc (vect_get_smallest_scalar_type): Special case
    	simd clone calls and only use types that are mapped to vectors.
    	(simd_clone_call_p): New helper function.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/vect/vect-simd-clone-16f.c: Remove unnecessary differentation
    	between targets with different pointer sizes.
    	* gcc.dg/vect/vect-simd-clone-17f.c: Likewise.
    	* gcc.dg/vect/vect-simd-clone-18f.c: Likewise.

--- gcc/testsuite/gcc.dg/vect/vect-simd-clone-16f.c
+++ gcc/testsuite/gcc.dg/vect/vect-simd-clone-16f.c
@@ -6,9 +6,8 @@
 #include "vect-simd-clone-16.c"
 
 /* Ensure the the in-branch simd clones are used on targets that support them.
-   Some targets use pairs of vectors and do twice the calls.  */
-/* { dg-final { scan-tree-dump-times {[\n\r] [^\n]* = foo\.simdclone} 2 "vect" { target { ! { i686*-*-* && { ! lp64 } } } } } } */
-/* { dg-final { scan-tree-dump-times {[\n\r] [^\n]* = foo\.simdclone} 4 "vect" { target { i686*-*-* && { ! lp64 } } } } } */
+ */
+/* { dg-final { scan-tree-dump-times {[\n\r] [^\n]* = foo\.simdclone} 2 "vect" } } */
 
 /* The LTO test produces two dump files and we scan the wrong one.  */
 /* { dg-skip-if "" { *-*-* } { "-flto" } { "" } } */
