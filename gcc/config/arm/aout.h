9ea54493300ecce6eb59827d6ac1cc57d437fc2c
Ilya Leoshkevich <iii@linux.ibm.com>
2023-12-07 13:08:26 +0100

    Implement ASM_DECLARE_FUNCTION_NAME using ASM_OUTPUT_FUNCTION_LABEL
    gccint recommends using ASM_OUTPUT_FUNCTION_LABEL in
    ASM_DECLARE_FUNCTION_NAME, but many implementations use
    ASM_OUTPUT_LABEL instead.  It's inconsistent and prevents changes to
    ASM_OUTPUT_FUNCTION_LABEL from affecting the respective targets.
    
    Link: https://inbox.sourceware.org/gcc-patches/20240102194511.3171559-2-iii@linux.ibm.com/
    Signed-off-by: Ilya Leoshkevich <iii@linux.ibm.com>
    
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64.cc (aarch64_declare_function_name):
    	Use ASM_OUTPUT_FUNCTION_LABEL ().
    	* config/alpha/alpha.cc (alpha_start_function): Likewise.
    	* config/arm/aout.h (ASM_DECLARE_FUNCTION_NAME): Likewise.
    	* config/arm/arm.cc (arm_asm_declare_function_name): Likewise.
    	* config/bfin/bfin.h (ASM_DECLARE_FUNCTION_NAME): Likewise.
    	* config/c6x/c6x.h (ASM_DECLARE_FUNCTION_NAME): Likewise.
    	* config/gcn/gcn.cc (gcn_hsa_declare_function_name): Likewise.
    	* config/h8300/h8300.h (ASM_DECLARE_FUNCTION_NAME): Likewise.
    	* config/ia64/ia64.cc (ia64_start_function): Likewise.
    	* config/mcore/mcore-elf.h (ASM_DECLARE_FUNCTION_NAME):
    	Likewise.
    	* config/microblaze/microblaze.cc (microblaze_function_prologue):
    	Likewise.
    	* config/mips/mips.cc (mips_start_unique_function): Return the
    	tree.
    	(mips_start_function_definition): Use
    	ASM_OUTPUT_FUNCTION_LABEL ().
    	(mips_finish_stub): Pass the tree to
    	mips_start_function_definition ().
    	(mips16_build_function_stub): Likewise.
    	(mips16_build_call_stub): Likewise.
    	(mips_output_function_prologue): Likewise.
    	* config/pa/pa.cc (pa_output_function_label): Use
    	ASM_OUTPUT_FUNCTION_LABEL ().
    	* config/riscv/riscv.cc (riscv_declare_function_name): Likewise.
    	* config/rs6000/rs6000.cc (rs6000_elf_declare_function_name):
    	Likewise.
    	(rs6000_xcoff_declare_function_name): Likewise.

--- gcc/config/arm/aout.h
+++ gcc/config/arm/aout.h
@@ -160,7 +160,7 @@
   do							\
     {							\
       ARM_DECLARE_FUNCTION_NAME (STREAM, NAME, DECL);   \
-      ASM_OUTPUT_LABEL (STREAM, NAME);			\
+      ASM_OUTPUT_FUNCTION_LABEL (STREAM, NAME, DECL);	\
     }							\
   while (0)
 #endif
